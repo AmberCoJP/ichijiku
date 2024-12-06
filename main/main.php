a951f75554d8f7d9f90e07fd34f4c912c650dc2c
takahashi-akane1418 <takahashi.amber0123@gmail.com>
2024-12-06 11:48:19 +0900

    first commit

---
+++ main/main.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+
+	<meta name="Keywords" content="豊中市,市軸稲荷神社,ご祈祷,厄除け,商売繁盛,安産祈願">
+	<meta name="Description" content="豊中市にある「市軸稲荷神社」の公式ページです。ご祈祷 厄除け 商売繁盛 安産祈願等お受けいたします。ご祈祷の受付は常時行っております。地鎮祭・各種清祓等、出張祭の受付も随時承っております。ご予約はお電話でもお受けいたしますのでお問合せください。">
+
+	<title>豊中市｜市軸稲荷神社｜ご祈祷｜厄除け｜商売繁盛｜安産祈願</title>
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.4/css/all.css">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/general.css" rel="stylesheet">
+	<link href="css/footer.css" rel="stylesheet">
+	<link href="css/spot.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/lightbox.css" type="text/css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/pagetop.js"></script>
+
+	<!-- スライドショーstart -->
+	<!-- スライドショーend -->
+	<script><!-- sp_menu click start -->
+		function menu() {
+			document.querySelector('html').classList.toggle('open');
+		}
+	</script><!-- sp_menu click end -->
+</head>
+
+<body>
+	<div id="page_top"><a href="#"></a></div>
+	<header class="header">
+		<h1>豊中市にある神社「市軸稲荷神社」では、ご祈祷 厄除け 商売繁盛 安産祈願等お受けいたします。</h1>
+		<div class="header-container w-container">
+			<div class="site">
+				<a href="https://ichijikuinarijinja.net/"><img src="img/logo.png" alt="市軸稲荷神社" width="340"></a>
+			</div>
+
+			<button class="navbtn" onClick="menu()">
+				<i class="fas fa-bars"></i>
+				<i class="fas fa-times"></i>
+				<span class="sr-only">MENU</span>
+			</button>
+
+			<div class="company">
+				<span class="emp">TEL.</span><a href="tel:0668573618" class="tel">06-6857-3618</a><br><p class="header-details">大阪府豊中市刀根山2丁目2番33号</p>
+			</div>
+		</div><!--header-container w-container end-->
+	</header>
+
+	<nav class="nav">
+		<ul>
+		<li onClick="menu()"><a href="https://ichijikuinarijinja.net/">市軸稲荷神社について</a></li>
+		<li onClick="menu()"><a href="https://ichijikuinarijinja.net/event.html">祭典行事</a></li>
+		<li onClick="menu()"><a href="https://ichijikuinarijinja.net/pray.html">御祈祷</a></li>
+		<li onClick="menu()"><a href="https://ichijikuinarijinja.net/info.html">交通アクセス</a></li>
+		<li onClick="menu()"><a href="https://ichijikuinarijinja.net/faq.html">よくあるご質問</a></li>
+		</ul>
+	</nav>
+
+	<section class="hero"><!-- メインビジュアル -->
+		<div class="hero-container w-container">
+
+			<picture class="main-v">
+				<source srcset="img/main-visual.jpg" media="(min-width: 569px)"/><!-- 幅569px以上なら表示 -->
+				<img src="img/main-visual_sp.jpg" alt="市軸稲荷神社"><!-- それ以外で表示 -->
+			</picture>
+
+		</div>
+	</section>
+
+
+
+	<section class="sec2">
+		<div class="w-container">
+			<br>
+			<p class="tx-c2 emp"><strong class="emp">市軸稲荷神社</strong>の公式ページをご覧頂きありがとうございます。</p>
+
+			<h2 class="lowerPage-heading-decoration">お知らせ</h2>
+			<hr class="no">
+
+			<div class="haba">
+				<div class="kukuri4">
+				<h4>【神道講話・書写】</h4>
+				<p>随時、受付しております。<br>宮司と楽しくお話ししながら、神道や神話、神様について知ることができます。<br>
+				Yahoo!ニュースに記事が掲載されました。<br>
+				<a href="https://news.yahoo.co.jp/expert/articles/6431d4fb309893603895773e22e107090ceecc38" target="_blank" class="aabb unli">https://news.yahoo.co.jp/expert/articles/6431d4fb309893603895773e22e107090ceecc38</a><br>
+				詳しくはお問い合わせください。</p>
+				</div>
+			</div><!--haba end-->
+
+			<hr class="no">
+		</div><!--w-container end-->
+	</section><!--sec2 end-->
+
+
+
+	<section class="sec1">
+		<div class="w-container">
+			<h2 class="lowerPage-heading-decoration">当神社について</h2>
+			<p class="subh"></p>
+			<img src="img/info_img01.jpg" alt="市軸稲荷神社"><br>
+			<p class="emp lihei18 tx-c3">大正15年 伏見稲荷大社 稲荷山の一ノ峰から御分霊を勧請し刀根山の地主神としてお祀りしたのが当社のはじまりです。<br>
+			この刀根山の地に御鎮座されてより〝<ruby>市軸<rt>いちじく</rt></ruby>さん〟〝刀根山のお稲荷さん〟として親しまれています。<br>
+			特に病気平癒・商売繁昌の神として篤い崇敬を受けてまいりました。<br>
+			とりわけ病気平癒・厄除・車祓のご祈願で名高く今なお全国から多くの方々が参拝に訪れています。</p>
+		</div><!--w-container end-->
+	</section><!--sec1 end-->
+
+
+
+	<section class="sec1">
+		<div class="w-container">
+
+			<hr class="decoline">
+			<div class="flex2">
+				<div class="flex-child03">
+					<div class="c-btn btn07 btwi"><a href="event.html" class="emp">祭典行事</a></div>
+					<img src="img/index_img01.jpg" alt="祭典行事" width="100%">
+				</div>
+				<div class="flex-child03">
+					<div class="c-btn btn07 btwi"><a href="pray.html" class="emp">御祈祷</a></div>
+					<img src="img/index_img02.jpg" alt="御祈祷" width="100%">
+				</div>
+			</div>
+			<hr class="no">
+		</div>
+	</section>
+
+
+
+
+	<section class="sec4">
+		<div class="w-container">
+			<hr class="decoline">
+			<dl>
+			<dt>一. 御本社</dt>
+			<dd>御祭神　<ruby>市軸大神<rt>いちじくおおかみ</rt></ruby>（稲荷大神）<dd>
+			<dd>相　殿　天照皇大神・ 豊受大神・松江大神（愛宕大神）・ 末廣大神（稲荷大神）・ 末光大神（稲荷大神）<dd>
+			</dl><br>
+
+			<dl>
+			<dt>一. 境内末社　お多福社</dt>
+			<dd>お多福大神（<ruby>瀨織津比咩大神<rt>せおりつひめおおかみ</rt></ruby>）<dd>
+			<dd>大祓詞の中に “速川の瀬に坐す神 ” としてみられるお祓いの神様（祓所四柱の神の一柱）<dd>
+			</dl><br>
+
+			<div class="flex2">
+				<div class="flex-child05">
+				<dl>
+				<dt>一. 境内末社　五社大明神<br class="sp"><span class="sp">　 </span>【全七社（全九柱）】</dt>
+				<dd>岩姫大神・［三草大神・五社大神・大山祇大神］・十柱大神・春日大神・中禅寺大神・若宮大神・三島大神<dd>
+				</dl>
+				</div>
+				<div class="flex-child04">
+					<img src="img/info_img02.jpg" alt="五社大明神 【全七社（全九柱）】" width="100%" class="maru">
+				</div>
+			</div>
+
+			<dl>
+			<dt>一. 境内末社　与吉社</dt>
+			<dd>与吉大明神（狸の神様）<br>一願成就の神様<dd>
+			</dl><br>
+
+
+			<div class="flex2">
+				<div class="flex-child03">
+					<p class="tx-c">御本社</p><img src="img/info_img03.jpg" alt="御本社" width="100%" class="maru">
+				</div>
+				<div class="flex-child03">
+					<p class="tx-c">お多福社</p><img src="img/info_img04.jpg" alt="お多福社" width="100%" class="maru">
+				</div>
+			</div>
+
+			<hr class="no">
+			<div class="flex2">
+				<div class="flex-child03">
+					<p class="tx-c">五社大明神</p><img src="img/info_img05.jpg" alt="五社大明神" width="100%" class="maru">
+				</div>
+				<div class="flex-child03">
+					<p class="tx-c">与吉社</p><img src="img/info_img06.jpg" alt="与吉社" width="100%" class="maru">
+				</div>
+			</div><br>
+			<hr class="no">
+		</div><!--w-container end-->
+	</section><!--sec4 end-->
+
+
+
+	<section class="sec3">
+		<div class="w-container">
+
+			<div class="contentsbg">
+
+			<h2 class="lowerPage-heading-decoration mb20 tx-les02">各種ご祈祷・出張祭の受付</h2>
+			<img src="img/imgillust_01.png" alt="祈祷イメージ玉串" class="pcspwid">
+			<p class="tx-c2 emp">ご祈祷の受付は常時行っております。<br>地鎮祭・各種清祓等　出張祭の受付も随時承っております。<br>
+			病気平癒・厄除・商売繁昌などに御利益があるとされています。<br>ご予約はお電話でもお受けいたしますのでお問い合わせください。</p><br>
+			<p class="c-arra2 f-xla empcolor">TEL　<a href="tel:0668573618">06-6857-3618</a></p>
+
+			</div><!--contentsbg end-->
+
+		</div><!--w-container end-->
+	</section><!--sec3 end-->
+
+
+
+
+	<footer class="footer">
+
+		<div class="pseudofooter">
+
+			<div class="footer-container w-container">
+
+				<div class="footer-site">
+					<ul class="footer-menu">
+					<li><a href="https://ichijikuinarijinja.net/">市軸稲荷神社について</a></li>
+					<li><a href="https://ichijikuinarijinja.net/event.html">祭典行事</a></li>
+					<li><a href="https://ichijikuinarijinja.net/pray.html">御祈祷</a></li>
+					<li><a href="https://ichijikuinarijinja.net/info.html">交通アクセス</a></li>
+					<li><a href="https://ichijikuinarijinja.net/faq.html">よくあるご質問</a></li>
+					</ul><br class="sp">
+				</div>
+
+				<div class="footer-sns">
+				<div class="spwi100"><a href="index.html"><span class="f-14">刀根山の地主神　</span><span class="f-20">市軸稲荷神社</span></a></div>
+				<span class="disinlb"><img src="img/f-bakicon2.png" width="40px"></span><span class="disinlb"><img src="img/f-bakicon.png" width="40px"></span>
+				<span class="disinlb"><a href="https://www.instagram.com/ichijikuinarijinja/" class="snsbtn" target="_blank"><i class="fab fa-instagram"></i><span class="sr-only">Instagram</span></a></span>
+				<br><br>
+				</div>
+				<p class="c-arra2"><span class="header-details">大阪府豊中市刀根山2丁目2番33号</span><br>TEL/FAX　<a href="tel:0668573618">06-6857-3618</a></p>
+
+			</div><!--footer-container w-container end-->
+
+		</div><!--pseudofooter 通常フッター部分end-->
+
+		<div class="footer-copy">
+			<p>&copy;2024-<span id="thisYear"></span>&nbsp;<a href="https://ichijikuinarijinja.net/" class="onmouse">市軸稲荷神社</a></p>
+		</div>
+
+	</footer>
+
+
+
+<script type="text/javascript">
+	date = new Date();
+	thisYear = date.getFullYear();
+	document.getElementById("thisYear").innerHTML = thisYear;
+</script>
+
+<script src="js/lightbox.js"></script>
+</body>
+</html>
